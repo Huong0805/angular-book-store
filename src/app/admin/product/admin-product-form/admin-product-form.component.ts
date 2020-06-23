16518666773ddd25d7b6c8a05e430bc72ae729af
Huong0805 <lethuhuong0805@gmail.com>
2020-06-24 00:18:23 +0700

    bai tap buoi 5

--- src/app/admin/product/admin-product-form/admin-product-form.component.ts
+++ src/app/admin/product/admin-product-form/admin-product-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/shared/services/product.service';
 import { Product } from 'src/app/shared/models/product';
+import { publishers } from 'src/app/shared/mock-data/publisher-list';
+
 @Component({
   selector: 'app-admin-product-form',
   templateUrl: './admin-product-form.component.html',
@@ -8,13 +10,20 @@ import { Product } from 'src/app/shared/models/product';
 })
 export class AdminProductFormComponent implements OnInit {
 
-  product: Product;
+  defaultTikiNow='false';
+  publishers=[];
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.publishers=publishers;
   }
 
-  addProduct(product:Product): void {
+  addProduct(addForm): void {
+    const publisher =publishers.find(ele=>ele.$key===addForm.value.publisher);
+    const product=new Product({
+      ...addForm.value,
+      publisher:publisher?publisher.value:''
+    });
     this.productService.createProduct(product);
   }
 }
